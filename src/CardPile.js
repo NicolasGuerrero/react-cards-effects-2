c4d65dcb3a0f7348817203b634e91135d3dc5fab
Adam <adamlevitz@gmail.com>
2020-02-28 15:37:22 -0800

    update part 1

--- src/CardPile.js
+++ src/CardPile.js
@@ -7,30 +7,9 @@ import './CardPile.css'
 const BASE_URL = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
 const CARD_URL = 'https://deckofcardsapi.com/api/deck/';
 
-
-
 function CardPile() {
   const [deck, setDeck] = useState(null);
   const [cardsArr, setCardsArr] = useState([]);
-  const renderCards = () => {
-    if(cardsArr[0] === undefined) {
-      return;
-    }
-    console.log(cardsArr);
-
-    return (
-      <div className="CardPile-renderCards">
-        {cardsArr.map((card, index) => (
-          <Card key={card.id}
-            id = {card.id}
-            image = {card.image}
-            index = {index}
-            remaining = {card.remaining}
-          />
-        ))}
-      </div>
-    );
-  };
 
   const addCard = card => {
     let newCard = { ...card, id: uuid() }
@@ -38,32 +17,53 @@ function CardPile() {
     setCardsArr(oldCards => [...oldCards, newCard]);
   }
 
+  // 2. use hook useEffect to fetch card data right after component renders
+  // 3. update state of deck
+  // 
   useEffect(() => {
     async function fetchDeck() {
       const deckResult = await axios.get(BASE_URL);
       setDeck(deckResult.data);
     };
     fetchDeck();
-  },[]);
-  
-  const handleCardDraw = () => { {
+  }, []);
+
+  const handleCardDraw = () => {
     async function fetchCard() {
       const cardResult = await axios.get(`${CARD_URL}${deck.deck_id}/draw/?count=1`);
-      if(cardResult.data.error){
+      if (cardResult.data.error) {
         alert(cardResult.data.error);
       } else {
-      
-      addCard(cardResult.data.cards[0]);
-      console.log(cardResult.data);
+
+        addCard(cardResult.data.cards[0]);
+        console.log(cardResult.data);
       }
     };
     fetchCard();
-    
-  };
-  
-
   }
 
+  const renderCards = () => {
+    if (cardsArr[0] === undefined) {
+      return null;
+    }
+    console.log(cardsArr);
+
+    return (
+      <div className="CardPile-renderCards">
+        {cardsArr.map((card, index) => (
+          <Card key={card.id}
+            id={card.id}
+            image={card.image}
+            index={index}
+            remaining={card.remaining}
+          />
+        ))}
+      </div>
+    );
+  };
+
+
+  // 1. Displays on initial render 
   return (
     <div>
       <p>{deck ? deck.deck_id : "Loading..."}</p>
